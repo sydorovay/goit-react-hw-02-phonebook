cb9fbd4802141ae44a28467c7bb2c80aeac8c1dc
Artem Sydorov <sydorovay@gmail.com>
2023-05-29 17:15:49 +0200

    add contactlist component

---
+++ src/components/contactList/ContactList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ContactList = ({ contacts, onDelete }) => {
+  return (
+    <ul>
+      {contacts.map(contact => (
+        <li key={contact.id}>
+          {contact.name}: {contact.number}
+          <button onClick={() => onDelete(contact.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
